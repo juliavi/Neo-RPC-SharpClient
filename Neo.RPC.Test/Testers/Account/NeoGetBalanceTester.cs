25d32def8f1d396a815ce88a83e64fd87f5a4ccd
Julia Vishnevsky <julia_v2@hotmail.com>
2018-03-08 14:33:59 +0200

    tests

---
+++ Neo.RPC.Test/Testers/Account/NeoGetBalanceTester.cs
@@ -0,0 +1,32 @@
+﻿using Neo.JsonRpc.Client;
+using Neo.RPC.DTOs;
+using Neo.RPC.Services.Account;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Neo.RPC.Tests.Testers.Account
+{
+    public class NeoGetBalanceTester : RpcRequestTester<WalletBalance>
+    {
+        const string NEOASSETID = "c56f33fc6ecfcd0c225c4ab356fee59390af8560be0e930faebe74a6daff7c9b";
+
+        [Fact]
+        public async void ShouldReturnWalletBalance()
+        {
+            var result = await ExecuteAsync();
+            Assert.NotNull(result);
+        }
+
+        public override async Task<WalletBalance> ExecuteAsync(IClient client)
+        {
+            var balance = new NeoGetBalance(client);
+            return await balance.SendRequestAsync(NEOASSETID);
+        }
+
+        public override Type GetRequestType()
+        {
+            return typeof(WalletBalance);
+        }
+    }
+}
---
+++ Neo.RPC.Test/Testers/Account/NeoGetNewAddressTester.cs
@@ -0,0 +1,29 @@
+﻿using Neo.JsonRpc.Client;
+using Neo.RPC.Services.Account;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Neo.RPC.Tests.Testers.Account
+{
+    public class NeoGetNewAddressTester : RpcRequestTester<string>
+    {
+        [Fact]
+        public async void ShouldReturnWalletBalance()
+        {
+            var result = await ExecuteAsync();
+            Assert.NotNull(result);
+        }
+
+        public override async Task<string> ExecuteAsync(IClient client)
+        {
+            var newAddress = new NeoGetNewAddress(client);
+            return await newAddress.SendRequestAsync();
+        }
+
+        public override Type GetRequestType()
+        {
+            return typeof(string);
+        }
+    }
+}
