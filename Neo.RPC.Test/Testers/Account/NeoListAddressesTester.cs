f064152eba5c748e5ecee23520c5ad55d22888b1
Julia Vishnevsky <julia_v2@hotmail.com>
2018-03-08 14:33:59 +0200

    tests

---
+++ Neo.RPC.Test/Testers/Account/NeoListAddressesTester.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Neo.JsonRpc.Client;
+using Neo.RPC.DTOs;
+using System.Threading.Tasks;
+using Xunit;
+using Neo.RPC.Services;
+
+namespace Neo.RPC.Tests.Testers.Account
+{
+    public class NeoListAddressesTester : RpcRequestTester<WalletAddress[]>
+    {
+        [Fact]
+        public async void ShouldReturnWalletBalance()
+        {
+            var result = await ExecuteAsync();
+            Assert.NotNull(result);
+        }
+
+        public override async Task<WalletAddress[]> ExecuteAsync(IClient client)
+        {
+            var listAddresses = new NeoListAddresses(client);
+            return await listAddresses.SendRequestAsync();
+        }
+
+        public override Type GetRequestType()
+        {
+            return typeof(WalletAddress[]);
+        }
+    }
+}
+
---
+++ Neo.RPC.Test/Testers/Transactions/NeoSendAssetsTester.cs
@@ -0,0 +1,33 @@
+﻿using Neo.JsonRpc.Client;
+using Neo.RPC.Services;
+using System;
+using System.Threading.Tasks;
+using System.Transactions;
+using Xunit;
+
+namespace Neo.RPC.Tests.Testers.Transactions
+{
+    public class NeoSendAssetsTester : RpcRequestTester<Transaction>
+    {
+        const string NEOASSETID = "c56f33fc6ecfcd0c225c4ab356fee59390af8560be0e930faebe74a6daff7c9b";
+        const string ADDRESS = "AK4if54jXjSiJBs6jkfZjxAastauJtjjse";
+
+        [Fact]
+        public async void ShouldReturnWalletBalance()
+        {
+            var result = await ExecuteAsync();
+            Assert.NotNull(result);
+        }
+
+        public override async Task<Transaction> ExecuteAsync(IClient client)
+        {
+            var sendAssets = new NeoSendAssets(client);
+            return await sendAssets.SendRequestAsync(NEOASSETID, ADDRESS, 1);
+        }
+
+        public override Type GetRequestType()
+        {
+            return typeof(Transaction);
+        }
+    }
+}
